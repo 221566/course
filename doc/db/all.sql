ba4a21d93fbbdaeb19b6d575b227876ab3f6eedb
221566 <2215668921@qq.com>
2023-03-09 21:29:12 +0800

    增加dto层，用于控制层和service层 修改chapterService将返回chapter改成返回chapterDto

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -5,4 +5,8 @@ create table `chapter` (
     `course_id` char(8) comment '课程ID',
     `name` varchar(50) comment '名称',
     primary key (`id`)
-)engine = innodb default charset = utf8mb4 comment ='大章'
+)engine = innodb default charset = utf8mb4 comment ='大章';
+
+insert into `chapter` (id,course_id,name) values ('00000000','00000000','测试大章一');
+insert into `chapter` (id,course_id,name) values ('00000001','00000000','测试大章二');
+
