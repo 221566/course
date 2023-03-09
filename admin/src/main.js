fb392d07549fc5d981b6dd6957fbb6c662858b7e
221566 <2215668921@qq.com>
2023-03-09 16:21:53 +0800

    安装vue-router

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,4 +1,11 @@
+import App from './app.vue'
+import router from './router'
 import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false;
+
+
+new Vue({
+    router,
+    render: h => h(App),
+}).$mount('#app');
